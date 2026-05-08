6a32089f0e766c51896dc9d71ccad0315019880a
ABDIRASHID <rashidsamadina@gmail.com>
2026-05-09 01:35:48 +0300

    Fix: remove debug prints

--- src/main.js
+++ src/main.js
@@ -4,19 +4,11 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const GITHUB_CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
 
-console.log(GITHUB_CLIENT_ID)
-
 document.getElementById('login-btn').addEventListener('click', () => {
   const authUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=read:user`;
-  console.log(GITHUB_CLIENT_ID)
   window.location.href = authUrl;
 });
 
-
-function getCsrfToken() {
-  const match = document.cookie.match(new RegExp('(^| )csrf_token=([^;]+)'));
-  return match ? match[2] : null;
-}
 
 async function init() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -70,9 +62,13 @@ async function fetchSecureData(isInitialCheck = false) {
 
       if (response.ok) {
           const data = await response.json();
-          document.getElementById("status-message").innerText = "✅ Authenticated successfully via HttpOnly Cookie!";
-          document.getElementById("secure-data").innerText = JSON.stringify(data, null, 2);
+          document.getElementById("status-message").innerText = "✅ Authenticated successfully";
+          document.getElementById("secure-data").innerText = JSON.stringify(data["username"], null, 2);
       } else {
+          
+           localStorage.removeItem("csrf_token");
+           document.getElementById("login-view").style.display = "block";
+           document.getElementById("dashboard").style.display = "none";
           if (!isInitialCheck) {
               document.getElementById("status-message").innerText = "Session expired or invalid CSRF.";
           } else {
